6e7bd9344c591ba6c00ad03c909316a603a8a2f4
zakharfsk <fesikzakhar@khmnu.edu.ua>
2023-02-07 21:52:08 +0200

    add type hinting

--- general_page/services/meal_api/meal_types.py
+++ general_page/services/meal_api/meal_types.py
@@ -2,18 +2,44 @@ from typing import NamedTuple
 
 
 class MealCategory(NamedTuple):
+    """
+    Meal category
+
+    :var id_category: str
+    :var name_category: str
+    :var category_thumb: str
+    """
     id_category: str
     name_category: str
     category_thumb: str
 
 
 class Meal(NamedTuple):
+    """
+    Meal
+
+    :var id_meal: str
+    :var meal_name: str
+    :var meal_thumb: str
+    """
     id_meal: str
     meal_name: str
     meal_thumb: str
 
 
 class MealDetail(NamedTuple):
+    """
+    Meal detail
+
+    :var meal_name: str
+    :var meal_thumb: str
+    :var meal_instructions: str
+    :var meal_area: str
+    :var meal_category: str
+    :var meal_tags: str
+    :var meal_youtube: str
+    :var meal_rate: str
+    """
     meal_name: str
     meal_thumb: str
     meal_instructions: str
