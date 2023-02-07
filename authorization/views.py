571405fb98e2d7b791a91edb9fc18896e23bcef9
zakharfsk <fesikzakhar@khmnu.edu.ua>
2023-02-07 18:05:08 +0200

    add type hinting and code rewrite

--- authorization/views.py
+++ authorization/views.py
@@ -1,11 +1,14 @@
+from django.http.response import HttpResponse
+
+from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render
 
 # Create your views here.
 
 
-def authorization_login(request):
+def authorization_login(request: WSGIRequest) -> HttpResponse:
     return render(request, 'authorization/login.html', {})
 
 
-def authorization_register(request):
+def authorization_register(request: WSGIRequest) -> HttpResponse:
     return render(request, 'authorization/register.html', {})
