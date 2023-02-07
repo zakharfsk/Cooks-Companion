044ae0bda08077698dd0c5ba1c7faa5a57562966
zakharfsk <fesikzakhar@khmnu.edu.ua>
2023-02-07 23:43:45 +0200

    big update

--- authorization/urls.py
+++ authorization/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import authorization_login, authorization_register
-
+from .views import authorization_login, authorization_register, authorization_logout
 
 urlpatterns = [
     path('register/', authorization_register, name='register_page'),
     path('login/', authorization_login, name='login_page'),
+    path('logout/', authorization_logout, name='logout_page'),
 ]
--- authorization/views.py
+++ authorization/views.py
@@ -1,14 +1,52 @@
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponse
 
 from django.core.handlers.wsgi import WSGIRequest
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
 
 def authorization_login(request: WSGIRequest) -> HttpResponse:
-    return render(request, 'authorization/login.html', {})
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'authorization/login.html', {'error': 'Invalid credentials'})
+
+    else:
+        return render(request, 'authorization/login.html', {})
 
 
 def authorization_register(request: WSGIRequest) -> HttpResponse:
-    return render(request, 'authorization/register.html', {})
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index_page')
+        else:
+            for msg in form.error_messages:
+                print(form.error_messages[msg])
+
+            return render(request, 'authorization/register.html', {'form': form})
+    else:
+        form = UserCreationForm()
+    return render(request, 'authorization/register.html', {'form': form})
+
+
+@login_required(login_url='login/')
+def authorization_logout(request: WSGIRequest) -> HttpResponse:
+    logout(request)
+    return redirect('index_page')
