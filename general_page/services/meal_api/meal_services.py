da6739f0eeb707fd9ebe53d202eb056a690ecd56
zakharfsk <fesikzakhar@khmnu.edu.ua>
2023-02-07 21:46:52 +0200

    update functional

--- general_page/services/meal_api/meal_services.py
+++ general_page/services/meal_api/meal_services.py
@@ -1,7 +1,74 @@
+from typing import Generator
+
 import requests
-from .meal_types import MealCategory
+from .meal_types import MealCategory, Meal, MealDetail
+from ..сhatsonic_api.сhatsonic_service import get_answer
 
 API_URL = 'https://www.themealdb.com/api/json/v1/1/'
+
+
+def get_meal_categories() -> Generator[MealCategory, MealCategory, None]:
+    """
+    Get meal categories from API themealdb
+
+    :return: Generator[MealCategory, MealCategory, None]
+    """
+
+    for category in _create_request('categories.php', '')['categories']:
+        yield MealCategory(
+            id_category=category['idCategory'],
+            name_category=category['strCategory'],
+            category_thumb=category['strCategoryThumb']
+        )
+
+
+def get_all_meal() -> Generator[Meal, Meal, None]:
+    """
+    Get all dishes from API themealdb
+
+    :return: Generator[Meal, Meal, None]
+    """
+    _categories = _get_list_categories()
+
+    for category in _categories:
+        for meal in get_all_meals_by_category(category):
+            yield meal
+
+
+def get_meal_by_id(meal_id: str) -> MealDetail:
+    """
+    Get meal by id from API themealdb
+
+    :param meal_id: str
+    :return: MealDetail
+    """
+    meal = _create_request('lookup.php', f'?i={meal_id}')['meals'][0]
+
+    return MealDetail(
+        meal_name=meal['strMeal'],
+        meal_thumb=meal['strMealThumb'],
+        meal_instructions=meal['strInstructions'].replace('\r\n', '<br>'),
+        meal_area=meal['strArea'],
+        meal_category=meal['strCategory'],
+        meal_tags=meal['strTags'],
+        meal_youtube=meal['strYoutube'],
+        meal_rate=get_answer(meal['strMeal']).rate,
+    )
+
+
+def get_all_meals_by_category(category: str) -> Generator[Meal, Meal, None]:
+    """
+    Get all meals by category
+
+    :param category: str
+    :return: Generator[Meal, Meal, None]
+    """
+    for meal in _create_request('filter.php', f'?c={category}')['meals']:
+        yield Meal(
+            id_meal=meal['idMeal'],
+            meal_name=meal['strMeal'],
+            meal_thumb=meal['strMealThumb'],
+        )
 
 
 def _create_request(type_: str, query: str) -> dict | None:
@@ -17,20 +84,10 @@ def _create_request(type_: str, query: str) -> dict | None:
     return response.json() if response.status_code == 200 else None
 
 
-def get_meal_categories() -> list[MealCategory] | None:
+def _get_list_categories() -> list[str]:
     """
-    Get meal categories from API themealdb
+    Get list categories from API themealdb
 
-    :return: list[dict] | None
+    :return: list[str]
     """
-
-    for category in _create_request('categories.php', '')['categories']:
-        yield MealCategory(
-            id_category=category['idCategory'],
-            name_category=category['strCategory'],
-            category_thumb=category['strCategoryThumb']
-        )
-
-
-def get_meal_by_id(meal_id: str) -> dict | None:
-    pass
+    return [category['strCategory'] for category in _create_request('list.php', '?c=list')['meals']]
--- general_page/views.py
+++ general_page/views.py
@@ -1,22 +1,23 @@
+import time
+
 from django.core.handlers.wsgi import WSGIRequest
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from .services.meal_api.meal_services import get_meal_categories
+from .services.meal_api.meal_services import get_meal_categories, get_all_meals_by_category, get_all_meal, \
+    get_meal_by_id
 
 
 # Create your views here.
 
 
 def index_page(request: WSGIRequest) -> HttpResponse:
-    categories = get_meal_categories()
-
     return render(
         request,
         'general_page/index.html',
         {
             'user': request.user,
-            'categories': categories,
+            'categories': get_meal_categories(),
         }
     )
 
@@ -29,13 +30,30 @@ def category_page(request: WSGIRequest) -> HttpResponse:
         'general_page/category.html',
         {
             'user': request.user,
+            'meal_category': get_all_meals_by_category(category),
         }
     )
 
 
 def search_page(request: WSGIRequest) -> HttpResponse:
-    return render(request, 'general_page/search.html', {})
+    return render(
+        request,
+        'general_page/search.html',
+        {
+            'user': request.user,
+            'meals': get_all_meal(),
+        }
+    )
 
 
 def view_page(request: WSGIRequest) -> HttpResponse:
-    return render(request, 'general_page/view.html', {})
+    meal_id = request.GET.get('id', '')
+
+    return render(
+        request,
+        'general_page/view.html',
+        {
+            'user': request.user,
+            'meal': get_meal_by_id(meal_id),
+        }
+    )
